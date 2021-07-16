d2a1d0363e059684b1f482c453393f7f92f2cc9f
maiconge <86364282+maiconge@users.noreply.github.com>
2021-07-16 15:29:59 -0300

    POO- PHP

--- Conta.php
+++ Conta.php
@@ -1,47 +1,83 @@
 <?php
 
-class Conta 
+class Conta
 {
-    public $cpfTitular;
-    public $nomeTitular;
+    private $cpfTitular;
+    private $nomeTitular;
     private $saldo;
+    private static $numeroDeContas = 0;
 
-    public function sacar(float $valorASacar): void
+    public function __construct(string $cpfTitular, string $nomeTitular)
     {
-        if ($valorAsacar > $this=>saldo) {
-            echo "Saldo indisponível";
+        $this->cpfTitular = $cpfTitular;
+        $this->validaNomeTitular($nomeTitular);
+        $this->nomeTitular = $nomeTitular;
+        $this->saldo = 0;
 
-        } else {
-            $this->saldo -= $valorASacar;
+        self::$numeroDeContas++;
+    }
+
+    public function __destruct()
+    {
+        self::$numeroDeContas--;
+    }
+
+    public function saca(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorASacar;
+    }
+
+    public function deposita(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
+
+    public function recuperaSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function recuperaCpfTitular(): string
+    {
+        return $this->cpfTitular;
+    }
+
+    public function recuperaNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
+
+    private function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
         }
     }
 
-        public function depositar(float |$valoraADepositar):void 
-        {
-            if ($valorADepositar > 0) {
-                $conta['saldo'] += $valorADepositar;
-
-            } else {
-                exibeMensagem (mensagem: 'Depositos precisam ser positivos');
-
-            }
-            return $conta;
-            
-        }
-        
-        public function transferir(float $valorATransferir, Conta $contaDestino): void
-        {
-            if ($valorATransferir > $this->saldo) {
-                echo "Saldo indisponível";
-                return;
-            }
-    
-            $this->sacar($valorATransferir);
-            $contaDestino->depositar($valorATransferir);
-        }
-
-
-
-
+    public static function recuperaNumeroDeContas(): int
+    {
+        return self::$numeroDeContas;
+    }
 }
-
